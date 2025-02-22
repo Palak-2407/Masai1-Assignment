1be36204358d722b4e862bbac93b8d04bba759b3
Palak-2407 <101452096+Palak-2407@users.noreply.github.com>
2025-02-22 22:04:55 +0530

    Add files via upload

---
+++ LoadingScreen.js
@@ -0,0 +1,12 @@
+console.log("Starting...");
+
+let count = 0;
+
+const loadingInterval = setInterval(() => {
+  console.log("Loading...");
+  count++;
+  if (count === 5) {
+    clearInterval(loadingInterval);
+    console.log("Loaded successfully!");
+  }
+}, 1000);
